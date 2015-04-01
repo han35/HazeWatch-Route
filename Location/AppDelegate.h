416ca5011ef6131115fdbdf3034c432947e63bbb
Pyay Han <pyayhan@Pyays-MacBook-Pro.local>
2015-04-01 17:03:42 +1100

    Initial Commit

---
+++ Location/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Location
+//
+//  Created by Pyay Han on 1/04/2015.
+//  Copyright (c) 2015 Pyay Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Location/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Location
+//
+//  Created by Pyay Han on 1/04/2015.
+//  Copyright (c) 2015 Pyay Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
